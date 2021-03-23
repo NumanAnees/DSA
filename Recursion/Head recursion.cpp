53450ae4403b0d01eb50d1c866cb6ebe011bd9fb
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-03-23 21:12:42 +0500

    updated

--- Recursion/Head recursion.cpp
+++ Recursion/Head recursion.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
+using namespace std;
 void fun(int n)
 {
- if(n>0)
- {
- fun(n-1);
- printf("%d ",n);
-
- }
+    if (n > 0)
+    {
+        fun(n - 1);
+        printf("%d ", n);
+    }
 }
-int main() {
- int x=3;
+int main()
+{
+    int x = 3;
 
- fun(x);
- return 0;
+    fun(x);
+    return 0;
 }
